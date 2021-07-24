6fbbea529400d8894c5226ef773b5e1bb24dfae1
Ezequiel Murillo <ezequiel.murillo@Ezequiels-MacBook-Pro.local>
2021-07-24 14:36:53 -0500

    First commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import json
+import logging
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    app.logger.info("Main request succesful")
+    return "Hello Docker!"
+
+if __name__ == "__main__":
+    logging.basicConfig(filename='app.log', level=logging.DEBUG)
+    
+    app.run(host='0.0.0.0')
